5febb9fbfab4df556dc4f028ea875187b42f5e3e
german9304 <31298672+german9304@users.noreply.github.com>
2019-05-05 17:35:31 -0700

    open card style

--- src/components/styles/openCardFormStyles.js
+++ src/components/styles/openCardFormStyles.js
@@ -3,7 +3,10 @@ import styled from 'styled-components';
 const OpenCardFormStyles = styled.section`
   // border: solid 1px black;
   margin: 0 0 0 0;
-  padding: 5px 6px 5px 6px;
+  padding: 8px 6px 5px 8px;
+  :hover {
+    background-color: rgba(0.1, 0.1, 0.1, 0.2);
+  }
 `;
 
 export default OpenCardFormStyles;
