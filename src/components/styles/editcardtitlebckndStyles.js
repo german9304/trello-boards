4f429a200981003bcb5e2944125cf223b73d01d0
german9304 <31298672+german9304@users.noreply.github.com>
2019-05-14 07:30:32 -0700

    background style

--- src/components/styles/editcardtitlebckndStyles.js
+++ src/components/styles/editcardtitlebckndStyles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
-const EditCardTitleBackGroundStyles = styled.section``;
+const EditCardTitleBackGroundStyles = styled.section`
+  position: relative;
+`;
 
 export default EditCardTitleBackGroundStyles;
