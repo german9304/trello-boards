23f253cc70739eb6bf407678ef2312492d8ba60d
german9304 <31298672+german9304@users.noreply.github.com>
2019-06-16 19:36:48 -0700

    init models

---
+++ backend/generated/prisma-client/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Board",
+    embedded: false
+  },
+  {
+    name: "Card",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `http://localhost:4466/trelloboards/api`
+});
+exports.prisma = new exports.Prisma();
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,2 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {});
+app.listen(PORT, () => console.log(`App listetning on port ${PORT}`));
